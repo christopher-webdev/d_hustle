83c0443506ff35c39d1f547c037830a1d39d46f5
Your Name <christopher_webdev@outlook.com>
2026-02-27 12:20:31 +0100

    s

--- math-game-backend/server.js
+++ math-game-backend/server.js
@@ -313,6 +313,14 @@ cron.schedule("0 0 * * 1", () => {
   });
 });
 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
